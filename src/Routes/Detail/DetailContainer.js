d509cbcbb63fe61cb6ca3fa91a168acbb4006eba
lsw1164 <qkqkfo@gmail.com>
2020-07-01 03:44:56 +0900

    Use Container Presenter Pattern to Search, TV, Detail

---
+++ src/Routes/Detail/DetailContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DetailPresenter from "./DetailPresenter";
+
+export default class extends React.Component {
+  state = {
+    result: null,
+    error: null,
+    loading: false,
+  };
+  render() {
+    const { result, error, loading } = this.state;
+    return <DetailPresenter result={result} error={error} loading={loading} />;
+  }
+}
---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import SearchPresenter from "./SearchPresenter";
+
+export default class extends React.Component {
+  state = {
+    movieResult: null,
+    tvResults: null,
+    searchTerm: "",
+    loading: false,
+    error: null,
+  };
+  render() {
+    const { movieResult, tvResults, searchTerm, loading, error } = this.state;
+    return (
+      <SearchPresenter
+        movieResult={movieResult}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        loading={loading}
+        error={error}
+      />
+    );
+  }
+}
