5ac5c0e8e87cc4e8356dcada826cf060a97d92db
lsw1164 <qkqkfo@gmail.com>
2020-07-06 23:24:16 +0900

    Call APIs in HomeContainer

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { moviesApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -9,6 +10,26 @@ export default class extends React.Component {
     error: null,
     loading: true,
   };
+
+  async componentDidMount() {
+    try {
+      this.setState({ loading: true });
+      const {
+        data: { results: popular },
+      } = await moviesApi.popular();
+      const {
+        data: { results: upcoming },
+      } = await moviesApi.upcoming();
+      const {
+        data: { results: nowPlaying },
+      } = await moviesApi.nowPlaying();
+      this.setState({ nowPlaying, popular, upcoming });
+    } catch {
+      this.setState({ error: "Can't find movie infomation." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
 
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
