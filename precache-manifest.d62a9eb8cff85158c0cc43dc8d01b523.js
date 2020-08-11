34a194d3f4a72970692cc87b47fc788d62a48b0f
swlee <swlee@bitmango.com>
2020-08-11 18:40:34 +0900

    Updates

---
+++ precache-manifest.d62a9eb8cff85158c0cc43dc8d01b523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583f76aeb5f6f01207390388448f8c0a",
+    "url": "/popcorntime-clone/index.html"
+  },
+  {
+    "revision": "1a7cc28669d871026fcf",
+    "url": "/popcorntime-clone/static/js/2.139ca613.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/popcorntime-clone/static/js/2.139ca613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de36b0089062d0949904",
+    "url": "/popcorntime-clone/static/js/main.c0816a11.chunk.js"
+  },
+  {
+    "revision": "22b38f17c21e7f78d9dd",
+    "url": "/popcorntime-clone/static/js/runtime-main.843101cb.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/popcorntime-clone/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
